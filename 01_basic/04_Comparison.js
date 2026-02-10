d53c5ce1b08de1fc1d9dce95d7767e4a200f6b3e
Rishi Dange <manishadange779@gmail.com>
2026-02-10 18:12:12 +0000

    todays commint

---
+++ 01_basic/04_Comparison.js
@@ -0,0 +1,21 @@
+/*console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+Isme hume true and false ka output aayega */
+//console.log("2" > 1);
+//console.log("02" > 1); //Isme wo integer mein convert karke de dega
+//console.log(null >= 0);
+//console.log(null > 0);
+//console.log(null == 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// ==
+
+console.log("2" === 2);
