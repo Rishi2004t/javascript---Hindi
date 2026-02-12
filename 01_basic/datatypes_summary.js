47bd48cec9cbdc911f3b44fa859a5e86858b4684
Rishi Dange <manishadange779@gmail.com>
2026-02-12 16:44:59 +0000

    fix

--- 01_basic/datatypes_summary.js
+++ 01_basic/datatypes_summary.js
@@ -2,7 +2,7 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
+/*const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
@@ -35,3 +35,27 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+*/
+//+++++++++++++++++++++++++++++++++++******************___________*********+++++++++++++++++
+
+//Stack (primitive) and Heap (non -primitive)
+
+let myInstaName = "vijaysuryawanshi28"
+
+let anotherName = "HiteshZuy"
+
+
+console.log(anotherName);
+console.log(myInstaName);
+console.log(anotherName + myInstaName);
+
+let user = {
+    email : "user@google.com",
+    upi : "@itjd34"
+}
+let userTwo = user
+userTwo.email = "rishidange420@gmail.com"
+
+console.log(user.email);
+console.log(userTwo.email);
